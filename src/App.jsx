2f0fa4927fcf656d7ad86f8ffe77c95ac36e7b05
luisr0514 <68670791+luisr0514@users.noreply.github.com>
2024-03-08 15:08:12 -0400

    implementacion de firebase
    aun falta

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+// importando los modulos de firebase
+import appFirabase from './credenciales'
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import './App.css'
+const auth = getAuth(appFirabase)
+//importaat componentes
+import Login from './componentes/Login'
+import Home from './componentes/Home'
+ 
+function App() {
+
+  const [usuario, setUsuario] = useState(null)
+  onAuthStateChanged(auth, (usuarioFirebase) => {
+
+    if (usuarioFirebase) {
+      setUsuario(usuarioFirebase)
+
+      
+    }
+    else
+    {
+      setUsuario(null)
+    }
+
+  })
+  
+
+  return (
+    <div>
+      {usuario ? <Home correoUsuario = {usuario.email}/> : <Login/>}
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import imagen from '../assets/login.png'
+import '../App.css'
+
+const Login = () => {
+  return (
+    <div className='container'>
+      <div className="row">
+        {/* columna pequeña para el formulario */}
+        <div className="col-mid-4">
+            <div className="padre">
+                <div className="card card-body">
+                  <form>
+                    <input type="text" placeholder='Ingresar correo electronico' />
+                    <input type="password" placeholder='Ingresar contraceña' />
+                    <button>Resgistrace</button>
+                  </form>
+
+
+                </div>
+            </div>
+
+        </div>
+        {/* columna mas grande */}
+        <div className="col-md-8">
+            <img src={imagen} alt="" className='tamaño-imagen' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login

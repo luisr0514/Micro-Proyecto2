3408bfca47a3566efaa6da0560501e1967ce8840
luisr0514 <68670791+luisr0514@users.noreply.github.com>
2024-03-11 23:32:30 -0400

    final
    final

--- src/componentes/SearchBar.jsx
+++ src/componentes/SearchBar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {FaSearch} from 'react-icons/fa'
+// import {FaSearch} from 'react-icons/fa'
 import './SearchBar.css'
 import Navbar from './navegación/Navbar';
 import { Link } from 'react-router-dom';
